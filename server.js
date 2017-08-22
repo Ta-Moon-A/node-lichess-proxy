944f3df0ccec72d94d9c4d070ab05291f1ee7c10
Dato <davit.bumbeishvili@gmail.com>
2017-08-22 22:35:45 +0400

    remove bodyparser

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', function (req, res) {
 
   var usernames = Object.keys(req.query)[0];
@@ -13,7 +12,7 @@ app.get('/', function (req, res) {
     url: 'https://lichess.org/api/users',
 
   }
-  
+
   request.post(options, (err, response, data) => {
     res.json(JSON.parse(data));
   })
